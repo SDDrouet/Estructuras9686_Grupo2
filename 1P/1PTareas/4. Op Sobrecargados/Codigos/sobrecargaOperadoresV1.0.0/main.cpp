6bdb25433d47624259ce0a2e2a97d38be0257b22
cabazurto1 <bazurtomora@gmail.com>
2023-05-17 23:28:28 -0500

    Op v1.0.2

--- 1P/1PTareas/4. Op Sobrecargados/Codigos/sobrecargaOperadoresV1.0.0/main.cpp
+++ 1P/1PTareas/4. Op Sobrecargados/Codigos/sobrecargaOperadoresV1.0.0/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char** argv) {
 	Operacion op1(5,4);
 	Operacion op2(6,11);
 	std::cout<<op1.getA() << "+"<<op1.getB()<<"+"<< op2.getA()<<"+"<<op2.getB()<<"="<<op1+op2<<std::endl; 
+	
+	// Operdor <<
 	ImpDato<int> dato1("Dato 1", 10);
     ImpDato<double> dato2("Dato 2", 3.14);
 
