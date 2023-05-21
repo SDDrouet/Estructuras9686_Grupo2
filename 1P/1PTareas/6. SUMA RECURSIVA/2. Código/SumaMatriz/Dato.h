2d7422c3fcf566ea8b95d15849a026e488623e76
cabazurto1 <bazurtomora@gmail.com>
2023-05-21 16:09:12 -0500

    Suma Mat recursiva

---
+++ 1P/1PTareas/6. SUMA RECURSIVA/2. Código/SumaMatriz/Dato.h
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * UNIVERSIDAD DE LAS FUERZAS ARMADAS - ESPE
+ * Nombres: Arias Sebastian, Bazurto Christopher, Carrera Nahir, Drouet Stephen
+ * Fecha de creacion: 09/05/23 16:54
+ * Fecha de modificacion: 20/05/23 23:09
+ * Enunciado:
+ * Ejemplo de punteros, matrices, suma y templates
+ * 
+ *******************************************************************************/ 
+
+#ifndef DATO_H
+#define DATO_H
+
+class Dato
+{
+	public:
+		static int ingresarMenuOpcion(char, char);
+		static int ingresarEntero(void);
+   		static float ingresarFloat(void);
+   		static int ingresarDimension(char cnum1, char cnum2);
+	protected:
+};
+
+#endif
